144b25b6bc70017c9bd593571481e910f67d775b
tienduy-nguyen <tienduy.nguyen.dev@gmail.com>
2021-08-07 17:13:21 +0200

    feat: some basic with println

--- crash-course/src/print.rs
+++ crash-course/src/print.rs
@@ -1,3 +1,29 @@
 pub fn run() {
+  // Print to console
   println!("Hello from print.rs file");
+
+  // Basic formatting
+  println!("Number: {}", 1);
+
+  // Positional arguments
+  println!(
+    "{0} is from {1} and {0} likes {2}",
+    "Tien Duy", "Vietnam", "the programing"
+  );
+
+  // Named arguments
+  println!(
+    "{name} likes to play {activity}",
+    name = "Tien Duy",
+    activity = "Ping pong"
+  );
+
+  // Placeholders traits
+  println!("Binary: {:b} Hex: {:x} Octal: {:o}", 10, 10, 10);
+
+  // Placeholders for debug traits
+  println!("{:?}", (12, true, "hello"));
+
+  // Basic math
+  println!("10 + 10 = {}", 10 + 10);
 }

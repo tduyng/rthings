a11c70cf0c683ce121ed62279ae5bd6e13d06d4f
tienduy-nguyen <tienduy.nguyen.dev@gmail.com>
2021-09-04 13:31:04 +0200

    feat(guessing_game): finish guessing game

--- rust-books/guessing_game/src/main.rs
+++ rust-books/guessing_game/src/main.rs
@@ -9,23 +9,31 @@ fn main() {
 
   println!("The secret number is: {}", secret_number);
 
-  println!("Please input your guess.");
+  loop {
+    println!("Please input your guess.");
 
-  // --snip--
+    let mut guess = String::new();
 
-  let mut guess = String::new();
+    io::stdin()
+      .read_line(&mut guess)
+      .expect("Failed to read line");
 
-  io::stdin()
-    .read_line(&mut guess)
-    .expect("Failed to read line");
+    let guess: u32 = match guess.trim().parse() {
+      Ok(num) => num,
+      Err(_) => continue,
+    };
 
-  let guess: u32 = guess.trim().parse().expect("Please type a number!");
+    println!("You guessed: {}", guess);
 
-  println!("You guessed: {}", guess);
+    // --snip--
 
-  match guess.cmp(&secret_number) {
-    Ordering::Less => println!("Too small!"),
-    Ordering::Greater => println!("Too big!"),
-    Ordering::Equal => println!("You win!"),
+    match guess.cmp(&secret_number) {
+      Ordering::Less => println!("Too small!"),
+      Ordering::Greater => println!("Too big!"),
+      Ordering::Equal => {
+        println!("You win!");
+        break;
+      }
+    }
   }
 }

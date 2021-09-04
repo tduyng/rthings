d50aa67aefa26feb941343f05cd4fe8d1412b018
tienduy-nguyen <tienduy.nguyen.dev@gmail.com>
2021-09-04 13:23:00 +0200

    feat(guessing_game): add first programme without loop

--- rust-books/guessing_game/src/main.rs
+++ rust-books/guessing_game/src/main.rs
@@ -1,4 +1,5 @@
 use rand::Rng;
+use std::cmp::Ordering;
 use std::io;
 
 fn main() {
@@ -10,11 +11,21 @@ fn main() {
 
   println!("Please input your guess.");
 
+  // --snip--
+
   let mut guess = String::new();
 
   io::stdin()
     .read_line(&mut guess)
     .expect("Failed to read line");
 
+  let guess: u32 = guess.trim().parse().expect("Please type a number!");
+
   println!("You guessed: {}", guess);
+
+  match guess.cmp(&secret_number) {
+    Ordering::Less => println!("Too small!"),
+    Ordering::Greater => println!("Too big!"),
+    Ordering::Equal => println!("You win!"),
+  }
 }

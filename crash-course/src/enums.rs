11ea010e7054055cc7725f76f220c364b25107d8
tienduy-nguyen <tienduy.nguyen.dev@gmail.com>
2021-09-05 16:50:29 +0200

    feat(test-crates): init project test crates

--- crash-course/src/enums.rs
+++ crash-course/src/enums.rs
@@ -1,4 +1,4 @@
-// Enums are types which have a few definite values
+// ## Enums are types which have a few definite values
 
 enum Movement {
   // Variants
@@ -29,3 +29,23 @@ pub fn run() {
   move_avatar(avatar3);
   move_avatar(avatar4);
 }
+
+// ## Concise control flow with if let
+
+fn main() {
+  let some_u8_value = Some(0u8);
+  match some_u8_value {
+    Some(3) => println!("three"),
+    _ => (),
+  }
+}
+
+// We can do with other way: more concise with if let
+// The syntax if let takes a pattern and an expression separated by an equal sign. 
+// It works the same way as a match, where the expression is given to the match and the pattern is its first arm.
+
+let some_u8_value = Some(0u8);
+if let Some(3) = some_u8_value {
+    println!("three");
+}
+

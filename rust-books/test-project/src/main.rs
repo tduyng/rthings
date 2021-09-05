499d86e184dd3a703859f02cd00724f5893b1371
tienduy-nguyen <tienduy.nguyen.dev@gmail.com>
2021-09-05 15:01:47 +0200

    feat(test-project): implement method for struct

--- rust-books/test-project/src/main.rs
+++ rust-books/test-project/src/main.rs
@@ -15,6 +15,10 @@ impl Rectangle {
   fn area(&self) -> u32 {
     self.width * self.height
   }
+
+  fn can_hold(&self, other: &Rectangle) -> bool {
+    self.width > other.width && self.height > other.height
+  }
 }
 
 fn main() {
@@ -27,6 +31,17 @@ fn main() {
     "The area of the rectangle is {} square pixels.",
     rect1.area()
   );
+  let rect2 = Rectangle {
+    width: 10,
+    height: 40,
+  };
+  let rect3 = Rectangle {
+    width: 60,
+    height: 45,
+  };
+
+  println!("Can rect1 hold rect2 ? {}", rect1.can_hold(&rect2));
+  println!("Can rect1 hold rect3 ? {}", rect1.can_hold(&rect3));
 }
 
 // fn main() {

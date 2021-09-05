6ac10f3e5d840cd6828310752848508855263027
tienduy-nguyen <tienduy.nguyen.dev@gmail.com>
2021-09-05 14:42:05 +0200

    feat(test-project): implement method for struct

--- rust-books/test-project/src/main.rs
+++ rust-books/test-project/src/main.rs
@@ -11,21 +11,39 @@ struct Rectangle {
   height: u32,
 }
 
+impl Rectangle {
+  fn area(&self) -> u32 {
+    self.width * self.height
+  }
+}
+
 fn main() {
-  let rect = Rectangle {
+  let rect1 = Rectangle {
     width: 30,
     height: 50,
   };
 
   println!(
-    "Area of rectangle with width: {0} and height: {1} is {2}",
-    rect.width,
-    rect.height,
-    get_area(&rect)
+    "The area of the rectangle is {} square pixels.",
+    rect1.area()
   );
-  println!("Rectangle is {:#?}", rect);
 }
 
-fn get_area(rect: &Rectangle) -> u32 {
-  rect.width * rect.height
-}
+// fn main() {
+//   let rect = Rectangle {
+//     width: 30,
+//     height: 50,
+//   };
+
+//   println!(
+//     "Area of rectangle with width: {0} and height: {1} is {2}",
+//     rect.width,
+//     rect.height,
+//     get_area(&rect)
+//   );
+//   println!("Rectangle is {:#?}", rect);
+// }
+
+// fn get_area(rect: &Rectangle) -> u32 {
+//   rect.width * rect.height
+// }

ed888225db4694f9ecaf311bb22b4641597b1d7f
tienduy-nguyen <tienduy.nguyen.dev@gmail.com>
2021-09-05 14:32:05 +0200

    feat(test-project): using struct

---
+++ rust-books/test-project/src/main.rs
@@ -0,0 +1,30 @@
+// #[derive(Debug)]
+// struct User {
+//   username: String,
+//   email: String,
+//   sign_in_count: u64,
+//   active: bool,
+// }
+#[derive(Debug)]
+struct Rectangle {
+  width: u32,
+  height: u32,
+}
+
+fn main() {
+  let rect = Rectangle {
+    width: 30,
+    height: 50,
+  };
+
+  println!(
+    "Area of rectangle with width: {0} and height: {1} is {2}",
+    rect.width,
+    rect.height,
+    get_area(&rect)
+  );
+}
+
+fn get_area(rect: &Rectangle) -> u32 {
+  rect.width * rect.height
+}

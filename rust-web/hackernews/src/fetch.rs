7aa87441fd1db532e323441ac85132826956296f
tienduy-nguyen <tienduy.nguyen.dev@gmail.com>
2023-09-14 09:45:11 +0200

    feat: remove all codes

--- rust-web/hackernews/src/fetch.rs
+++
@@ -1,8 +0,0 @@
-use super::rss;
-use rss::{Channel, Item};
-
-pub type FetchResult<T> = Result<T, rss::Error>;
-
-pub fn fetch_from(url: &str) -> FetchResult<Vec<Item>> {
-  Ok(Channel::from_str(url)?.items().to_vec())
-}

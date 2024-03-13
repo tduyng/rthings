8ee1f971d6d6649693e0966a83cdf44e34d14af8
tienduy-nguyen <tienduy.nguyen.dev@gmail.com>
2024-03-13 14:51:31 +0100

    feat(gentype): add first example

--- gentype/src/main.rs
+++ gentype/src/main.rs
@@ -1,3 +1,31 @@
+use clap::{Arg, Command};
+
 fn main() {
-    println!("Hello, world!");
+    let matches = Command::new("Gentype")
+        .version("0.1.0")
+        .author("Felix N")
+        .about("Covert Rust types to Typescript types")
+        .arg(
+            Arg::new("input")
+                .short('i')
+                .long("input")
+                .required(true)
+                .help("The Rust file to process (including ext"),
+        )
+        .arg(
+            Arg::new("output")
+                .short('o')
+                .long("output")
+                .required(true)
+                .help("The name of Typescript file to output (including ext"),
+        )
+        .get_matches();
+
+    let input_filename = matches.get_one::<String>("input").expect("input required");
+    let output_filename = matches
+        .get_one::<String>("output")
+        .expect("output required");
+
+    dbg!(input_filename);
+    dbg!(output_filename);
 }

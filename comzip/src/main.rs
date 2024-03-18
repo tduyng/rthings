a02f91bf5818fcf9acb2c99a4a5e3f2f14c4c9f6
tienduy-nguyen <tienduy.nguyen.dev@gmail.com>
2024-03-18 17:30:13 +0100

    feat(comzip): compress folder

--- comzip/src/main.rs
+++ comzip/src/main.rs
@@ -1,34 +1,47 @@
+use std::fs;
 use std::fs::File;
 use std::io::{Read, Write};
+use std::path::Path;
 use zip::write::FileOptions;
 use zip::write::ZipWriter;
 use zip::CompressionMethod;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    // Open the file
-    let mut file = File::open("docs/lorem.txt")?;
-
-    // Read file content
-    let mut file_content = Vec::new();
-    file.read_to_end(&mut file_content)?;
+    let folder_path = "docs";
 
     // Create the zip file
     let file_zip = File::create("archive.zip")?;
     let mut zip = ZipWriter::new(file_zip);
 
-    // Define compression options
-    let options = FileOptions::default()
-        .compression_method(CompressionMethod::Stored)
-        .unix_permissions(0o755); // Adjust permissions as needed
+    compress_folder(folder_path, &mut zip)?;
 
-    // Start adding file to the zip
-    zip.start_file("lorem.txt", options)?;
-
-    // Write the file content to the zip
-    zip.write_all(&file_content)?;
-
-    // Finish writing the zip
     zip.finish()?;
+    Ok(())
+}
+
+fn compress_folder<P: AsRef<Path>>(
+    folder_path: P,
+    zip_writer: &mut ZipWriter<File>,
+) -> Result<(), Box<dyn std::error::Error>> {
+    let folder_path = folder_path.as_ref();
+    let options = FileOptions::default()
+                .compression_method(CompressionMethod::Stored)
+                .unix_permissions(0o755);
+
+    for entry in fs::read_dir(folder_path)? {
+        let entry = entry?;
+        let path = entry.path();
+
+        if path.is_file() {
+            let file_name = path.file_name().unwrap().to_string_lossy().into_owned();
+            let mut file = File::open(&path)?;
+            let mut file_content = Vec::new();
+            file.read_to_end(&mut file_content)?;
+
+            zip_writer.start_file(file_name, options)?;
+            zip_writer.write_all(&file_content)?;
+        }
+    }
 
     Ok(())
 }

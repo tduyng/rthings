312e735909a1d80c7731ce348e2314a8ce559bfd
tienduy-nguyen <tienduy.nguyen.dev@gmail.com>
2024-03-20 22:46:27 +0100

    refactor(minigrep): split parse config

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -3,8 +3,7 @@ use std::{env, fs};
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let query = &args[1];
-    let file_path = &args[2];
+    let (query, file_path) = parse_config(&args);
 
     println!("Searching for {}", query);
     println!("In file {}", file_path);
@@ -12,4 +11,15 @@ fn main() {
     let contents = fs::read_to_string(file_path).expect("Should have been able to read the file");
 
     println!("With text:\n{contents}");
+}
+
+struct Config {
+   query: String,
+   file_path: String,
+}
+fn parse_config(args: &[String]) -> (&str, &str) {
+    let query = &args[1];
+    let file_path = &args[2];
+
+    (query, file_path)
 }

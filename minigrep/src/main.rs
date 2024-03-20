6ed08e03787b7bab85ab887af32d0c0fdba5c17e
tienduy-nguyen <tienduy.nguyen.dev@gmail.com>
2024-03-20 22:54:49 +0100

    refactor(minigrep): use struct constructer

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -3,7 +3,7 @@ use std::{env, fs};
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let config = parse_config(&args);
+    let config = Config::new(&args);
 
     println!("Searching for {}", config.query);
     println!("In file {}", config.file_path);
@@ -18,9 +18,12 @@ struct Config {
     query: String,
     file_path: String,
 }
-fn parse_config(args: &[String]) -> Config {
-    let query = args[1].clone();
-    let file_path = args[2].clone();
 
-    Config { query, file_path }
+impl Config {
+   fn new(args: &[String]) -> Config {
+      let query = args[1].clone();
+      let file_path = args[2].clone();
+
+      Config {query, file_path}
+   }
 }

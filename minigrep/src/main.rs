a2572ffb1544f7020214ae07a666dfa3288cfbc7
tienduy-nguyen <tienduy.nguyen.dev@gmail.com>
2024-03-20 23:01:37 +0100

    refactor(minigrep): handle error with result

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -1,9 +1,13 @@
+use std::process;
 use std::{env, fs};
 
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let config = Config::new(&args);
+    let config = Config::build(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments: {err}");
+        process::exit(1);
+    });
 
     println!("Searching for {}", config.query);
     println!("In file {}", config.file_path);
@@ -20,13 +24,13 @@ struct Config {
 }
 
 impl Config {
-   fn new(args: &[String]) -> Config {
-      if args.len() < 3 {
-         panic!("Not enough arguments");
-      }
-      let query = args[1].clone();
-      let file_path = args[2].clone();
+    fn build(args: &[String]) -> Result<Config, &'static str> {
+        if args.len() < 3 {
+            return Err("Not enough arguments");
+        }
+        let query = args[1].clone();
+        let file_path = args[2].clone();
 
-      Config {query, file_path}
-   }
+        Ok(Config { query, file_path })
+    }
 }

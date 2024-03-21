6604391e077e61d837b7087fccec36ab51457c47
tienduy-nguyen <tienduy.nguyen.dev@gmail.com>
2024-03-21 16:05:45 +0100

    feat(rename_ts): use clap

--- rename_ts/src/main.rs
+++ rename_ts/src/main.rs
@@ -1,42 +1,53 @@
-use std::env;
+use clap::Parser;
 use std::fs;
 use std::path::Path;
 
+#[derive(Parser)]
+#[command(name = "MyApp")]
+#[command(version = "1.0")]
+#[command(about = "Move and rename Typescript files", long_about = None)]
+struct Cli {
+    #[arg(long)]
+    folder: String,
+    #[arg(long)]
+    ext: String,
+}
+
 fn main() {
-    // Get the command-line arguments
-    let args: Vec<String> = env::args().collect();
+    let cli = Cli::parse();
 
-    // Check if the required number of arguments is provided
-    if args.len() != 3 {
-        println!("Usage: {} <folder_path> <file_extension>", args[0]);
-        return;
+    if let Err(err) = process_files(&cli.folder, &cli.ext) {
+        eprintln!("Error: {}", err);
     }
+}
 
-    // Extract the folder path and file extension from the arguments
-    let folder_path = &args[1];
-    let file_extension = &args[2];
-
+fn process_files(folder_path: &str, file_extension: &str) -> Result<(), String> {
     // Read directory
-    if let Ok(entries) = fs::read_dir(folder_path) {
-        for entry in entries.flatten() {
-            let file_name = entry.file_name();
-            let file_name_str = file_name.to_string_lossy();
+    let entries =
+        fs::read_dir(folder_path).map_err(|e| format!("Failed to read directory: {}", e))?;
 
-            // Check if the file has the expected extension
-            if file_name_str.ends_with(file_extension) {
-                // Extract the folder name
-                let folder_name = file_name_str.trim_end_matches(file_extension);
+    for entry in entries.flatten() {
+        let file_name = entry.file_name();
+        let file_name_str = file_name.to_string_lossy();
 
-                // Create directory if it doesn't exist
-                let dir_path = format!("{}/{}", folder_path, folder_name);
-                if !Path::new(&dir_path).exists() {
-                    fs::create_dir(&dir_path).expect("Failed to create directory");
-                }
+        // Check if the file has the expected extension
+        if file_name_str.ends_with(file_extension) {
+            // Extract the folder name
+            let folder_name = file_name_str.trim_end_matches(file_extension);
 
-                // Move the file
-                let new_file_path = format!("{}/index{}", dir_path, file_extension);
-                fs::rename(entry.path(), new_file_path).expect("Failed to move file");
+            // Create directory if it doesn't exist
+            let dir_path = format!("{}/{}", folder_path, folder_name);
+            if !Path::new(&dir_path).exists() {
+                fs::create_dir(&dir_path)
+                    .map_err(|e| format!("Failed to create directory: {}", e))?;
             }
+
+            // Move the file
+            let new_file_path = format!("{}/index{}", dir_path, file_extension);
+            fs::rename(entry.path(), &new_file_path)
+                .map_err(|e| format!("Failed to move file: {}", e))?;
         }
     }
+
+    Ok(())
 }

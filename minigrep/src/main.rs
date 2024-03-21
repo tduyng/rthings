9b34aeffd9f57a5eb91f562f7d21928166383957
tienduy-nguyen <tienduy.nguyen.dev@gmail.com>
2024-03-21 09:11:00 +0100

    feat(minigrep): handle standard error

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -6,15 +6,15 @@ fn main() {
     let args: Vec<String> = env::args().collect();
 
     let config = Config::build(&args).unwrap_or_else(|err| {
-        println!("Problem parsing arguments: {err}");
+        eprintln!("Problem parsing arguments: {err}");
         process::exit(1);
     });
 
-    println!("Searching for {}", config.query);
-    println!("In file {}", config.file_path);
+    eprintln!("Searching for {}", config.query);
+    eprintln!("In file {}", config.file_path);
 
     if let Err(e) = minigrep::run(config) {
-        println!("Application error: {e}");
+        eprintln!("Application error: {e}");
         process::exit(1);
     }
 }

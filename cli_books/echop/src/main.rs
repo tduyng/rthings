fff823e9f9788957b8d2cd55451ae8f2455e9970
tienduy-nguyen <tienduy.nguyen.dev@gmail.com>
2024-03-25 19:25:32 +0100

    feat(echop): make command and args cli with clap

--- cli_books/echop/src/main.rs
+++ cli_books/echop/src/main.rs
@@ -1,3 +1,24 @@
+use clap::Parser;
+
+#[derive(Debug, Parser)]
+#[command(author, version, about)]
+/// `echo` plus written in rust
+struct Args {
+    /// Input text
+    #[arg(required(true))]
+    text: Vec<String>,
+
+    /// Do not print newline
+    #[arg(short('n'))]
+    omit_newline: bool,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Args::parse();
+
+    print!(
+        "{}{}",
+        args.text.join(" "),
+        if args.omit_newline { " " } else { "\n" }
+    );
 }

2e8ba9f6e183905764222e08d695c5a3bd914802
tienduy-nguyen <tienduy.nguyen.dev@gmail.com>
2024-03-26 20:56:49 +0100

    chore(newsletter): disable sql code

--- newsletter/src/routes/subscriptions.rs
+++ newsletter/src/routes/subscriptions.rs
@@ -1,32 +1,38 @@
-use actix_web::{web, HttpResponse};
-use chrono::Utc;
-use sqlx::PgPool;
-use uuid::Uuid;
+// use actix_web::{web, HttpResponse};
+// use sqlx::PgPool;
+// use chrono::Utc;
+// use uuid::Uuid;
+
+use actix_web::HttpResponse;
 
 #[derive(serde::Deserialize)]
+#[allow(dead_code)]
 pub struct FormData {
     email: String,
     name: String,
 }
 
-pub async fn subscribe(form: web::Form<FormData>, pool: web::Data<PgPool>) -> HttpResponse {
-    match sqlx::query!(
-        r#"
-    INSERT INTO subscriptions (id, email, name, subscribed_at)
-    VALUES ($1, $2, $3, $4)
-            "#,
-        Uuid::new_v4(),
-        form.email,
-        form.name,
-        Utc::now()
-    )
-    .execute(pool.as_ref())
-    .await
-    {
-        Ok(_) => HttpResponse::Ok().finish(),
-        Err(e) => {
-            println!("Failed to execute query: {}", e);
-            HttpResponse::InternalServerError().finish()
-        }
-    }
+#[allow(dead_code)]
+// pub async fn subscribe(form: web::Form<FormData>, pool: web::Data<PgPool>) -> HttpResponse {
+pub async fn subscribe() -> HttpResponse {
+    // match sqlx::query!(
+    //     r#"
+    // INSERT INTO subscriptions (id, email, name, subscribed_at)
+    // VALUES ($1, $2, $3, $4)
+    //         "#,
+    //     Uuid::new_v4(),
+    //     form.email,
+    //     form.name,
+    //     Utc::now()
+    // )
+    // .execute(pool.as_ref())
+    // .await
+    // {
+    //     Ok(_) => HttpResponse::Ok().finish(),
+    //     Err(e) => {
+    //         println!("Failed to execute query: {}", e);
+    //         HttpResponse::InternalServerError().finish()
+    //     }
+    // }
+    HttpResponse::Ok().finish()
 }
--- newsletter/tests/health.rs
+++ newsletter/tests/health.rs
@@ -80,13 +80,13 @@ async fn subscribe_returns_200_for_valid_form_data() {
 
     assert_eq!(200, response.status().as_u16());
 
-    let saved = sqlx::query!("SELECT email, name FROM subscriptions",)
-        .fetch_one(&app.db_pool)
-        .await
-        .expect("Failed to fetch saved subscription.");
+    // let saved = sqlx::query!("SELECT email, name FROM subscriptions",)
+    //     .fetch_one(&app.db_pool)
+    //     .await
+    //     .expect("Failed to fetch saved subscription.");
 
-    assert_eq!(saved.email, "ursula_le_guin@gmail.com");
-    assert_eq!(saved.name, "le guin");
+    // assert_eq!(saved.email, "ursula_le_guin@gmail.com");
+    // assert_eq!(saved.name, "le guin");
 }
 
 #[tokio::test]

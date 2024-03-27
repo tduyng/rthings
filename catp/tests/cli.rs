ead65ebd89294616bd38fb45ada3ae9775f82cfe
tienduy-nguyen <tienduy.nguyen.dev@gmail.com>
2024-03-27 13:59:20 +0100

    test(catp): add more test

---
+++ catp/tests/cli.rs
@@ -0,0 +1,49 @@
+use assert_cmd::Command;
+use predicates::prelude::*;
+use rand::{distributions::Alphanumeric, Rng};
+use std::{error::Error, fs};
+
+const PRG: &str = "catp";
+const EMPTY: &str = "tests/inputs/empty.txt";
+const FOX: &str = "tests/inputs/fox.txt";
+const SPIDERS: &str = "tests/inputs/spiders.txt";
+const BUSTLE: &str = "tests/inputs/the-bustle.txt";
+
+#[test]
+fn usage() -> Result<(), Box<dyn Error>> {
+    for flag in &["-h", "--help"] {
+        Command::cargo_bin(PRG)?
+            .arg(flag)
+            .assert()
+            .stdout(predicate::str::contains("Usage"));
+    }
+
+    Ok(())
+}
+
+#[allow(dead_code)]
+fn gen_bad_file() -> String {
+    loop {
+        let filename: String = rand::thread_rng()
+            .sample_iter(&Alphanumeric)
+            .take(7)
+            .map(char::from)
+            .collect();
+
+        if fs::metadata(&filename).is_err() {
+            return filename;
+        }
+    }
+}
+
+#[test]
+fn skips_bad_file() -> Result<(), Box<dyn Error>> {
+    let bad = gen_bad_file();
+    let expected = format!("{bad}: .* [(]os error 2[)]");
+    Command::cargo_bin(PRG)?
+        .arg(&bad)
+        .assert()
+        .success()
+        .stderr(predicate::str::is_match(expected)?);
+    Ok(())
+}

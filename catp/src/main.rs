26f2638e2a02069ff9834ed10da44a76dcd1c5af
tienduy-nguyen <tienduy.nguyen.dev@gmail.com>
2024-03-27 08:47:55 +0100

    feat(catp): comlete cat feature

--- catp/src/main.rs
+++ catp/src/main.rs
@@ -1,3 +1,66 @@
+use std::{
+    error::Error,
+    fs::File,
+    io::{self, BufRead, BufReader},
+};
+
+use clap::Parser;
+
 fn main() {
-    println!("Hello, world!");
+    if let Err(e) = run(Args::parse()) {
+        eprintln!("{}", e);
+        std::process::exit(1);
+    }
+}
+#[derive(Debug, Parser)]
+#[command(author, version, about)]
+/// `cat` implement by rust
+struct Args {
+    /// Input file(s)
+    #[arg(value_name = "FILE", default_value = "-")]
+    files: Vec<String>,
+
+    /// Number lines
+    #[arg(short('n'), long("number"), conflicts_with("number_nonblank_lines"))]
+    number_lines: bool,
+
+    /// Number non-blank lines
+    #[arg(short('b'), long("number-nonblank"))]
+    number_nonblank_lines: bool,
+}
+
+fn run(args: Args) -> Result<(), Box<dyn Error>> {
+    for filename in args.files {
+        match open(&filename) {
+            Err(e) => eprintln!("{filename}: {e}"),
+            Ok(file) => {
+                let mut prev_num = 0;
+                for (line_num, line_result) in file.lines().enumerate() {
+                    let line = line_result?;
+                    if args.number_lines {
+                        println!("{:6}\t{line}", line_num + 1);
+                    } else if args.number_nonblank_lines {
+                        if line.is_empty() {
+                            println!();
+                        } else {
+                            prev_num += 1;
+                            println!("{prev_num:6}\t{line}");
+                        }
+                    } else {
+                        println!("{line}");
+                    }
+                }
+            }
+        }
+    }
+
+    Ok(())
+}
+
+#[allow(dead_code)]
+fn open(filename: &str) -> Result<Box<dyn BufRead>, Box<dyn Error>> {
+    match filename {
+        "-" => Ok(Box::new(BufReader::new(io::stdin()))),
+        _ => Ok(Box::new(BufReader::new(File::open(filename)?))),
+    }
 }

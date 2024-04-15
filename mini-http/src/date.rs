e3f6d2268a882b0e689d14da03758d78d8f1adf2
tienduy-nguyen <tienduy.nguyen.dev@gmail.com>
2024-04-15 19:33:21 +0200

    feat(mini-http): delete project, see more at rhttp repo

--- mini-http/src/date.rs
+++
@@ -1,5 +0,0 @@
-use chrono::Local;
-
-pub fn now() -> String {
-    Local::now().to_rfc2822()
-}
--- mini-http/src/lib.rs
+++
@@ -1,6 +0,0 @@
-pub mod date;
-pub mod request;
-pub mod response;
-
-pub type Error = Box<dyn std::error::Error + Send + Sync>;
-pub type Result<T> = std::result::Result<T, Error>;

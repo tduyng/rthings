a383db2ca0e7d7b35f26d4114050af302a0cc58b
tienduy-nguyen <tienduy.nguyen.dev@gmail.com>
2024-04-26 12:18:35 +0200

    feat: add breadth first search solution

--- readir/src/dir.rs
+++ readir/src/dir.rs
@@ -1,6 +1,6 @@
 use crate::Result;
 use core::fmt;
-use std::{fs, path::PathBuf};
+use std::{collections::VecDeque, fs, path::PathBuf};
 
 pub struct DirEntry {
     pub root: PathBuf,
@@ -42,6 +42,29 @@ impl DirEntry {
                 }
             }
         }
+        Ok(())
+    }
+
+    #[allow(dead_code)]
+    fn read_entries_bfs(entries: &mut Vec<PathBuf>, dir: &PathBuf) -> Result<()> {
+        let mut queue = VecDeque::new();
+        queue.push_back(dir.clone());
+
+        while let Some(current_dir) = queue.pop_front() {
+            if let Ok(entries_iter) = fs::read_dir(&current_dir) {
+                for entry in entries_iter {
+                    let entry = entry?;
+                    let path = entry.path();
+                    let is_dir = path.is_dir();
+
+                    entries.push(path.clone());
+                    if is_dir {
+                        queue.push_back(path)
+                    }
+                }
+            }
+        }
+
         Ok(())
     }
 }

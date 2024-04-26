20bf8f895f86902051751acb6db58724a9e0f946
tienduy-nguyen <tienduy.nguyen.dev@gmail.com>
2024-04-26 11:18:40 +0200

    feat(readir): add display all option

--- readir/src/dir.rs
+++ readir/src/dir.rs
@@ -10,21 +10,35 @@ pub struct DirEntry {
 impl DirEntry {
     pub fn new(root: PathBuf) -> Result<Self> {
         let mut child_paths = Vec::new();
-        DirEntry::read_entries(&mut child_paths, &root, &root)?;
+        DirEntry::read_entries(&mut child_paths, &root)?;
         Ok(DirEntry { root, child_paths })
     }
 
-    fn read_entries(entries: &mut Vec<PathBuf>, dir: &PathBuf, root: &PathBuf) -> Result<()> {
+    pub fn list_files_in_root(&self, include_nested: bool) -> Vec<&PathBuf> {
+        if include_nested {
+            self.child_paths.iter().filter(|p| !p.is_dir()).collect()
+        } else {
+            self.child_paths
+                .iter()
+                .filter(|p| !p.is_dir() && p.parent() == Some(&self.root))
+                .collect()
+        }
+    }
+
+    pub fn list_all_files(&self) -> &Vec<PathBuf> {
+        &self.child_paths
+    }
+
+    fn read_entries(entries: &mut Vec<PathBuf>, dir: &PathBuf) -> Result<()> {
         if let Ok(entries_iter) = fs::read_dir(dir) {
             for entry in entries_iter {
-                if let Ok(entry) = entry {
-                    let path = entry.path();
-                    let is_dir = path.is_dir();
+                let entry = entry?;
+                let path = entry.path();
+                let is_dir = path.is_dir();
 
-                    entries.push(path.clone());
-                    if is_dir {
-                        DirEntry::read_entries(entries, &path, root)?;
-                    }
+                entries.push(path.clone());
+                if is_dir {
+                    DirEntry::read_entries(entries, &path)?;
                 }
             }
         }
--- readir/src/main.rs
+++ readir/src/main.rs
@@ -8,20 +8,37 @@ use readir::{dir::DirEntry, Result};
 struct CliArgs {
     #[clap(short, long)]
     directory: PathBuf,
+
+    #[clap(short, long, default_value = "false")]
+    all: bool,
 }
 
 fn main() -> Result<()> {
     let args = CliArgs::parse();
     let dir = args.directory;
+    let display_all = args.all;
 
     if !dir.is_dir() {
         eprintln!("Error: provided is not a directory");
         std::process::exit(1);
     }
 
-    println!("Listing files in directory: {}", dir.display());
-    let entry = DirEntry::new(dir)?;
-    println!("{}", entry);
+    let entry = DirEntry::new(dir.clone())?;
+    if args.all {
+        println!(
+            "Listing all files in directory and subdirectories: {}",
+            dir.display()
+        );
+        println!("{}", entry);
+    } else {
+        println!("Listing files in directory: {}", dir.display());
+        for file in entry.list_files_in_root(display_all) {
+            match file.strip_prefix(&dir) {
+                Ok(relative_path) => println!("{}", relative_path.display()),
+                Err(_) => println!("{}", file.display()),
+            }
+        }
+    }
 
     Ok(())
 }

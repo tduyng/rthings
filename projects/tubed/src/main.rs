5c5af2da4b2508c5ef79b84e26f8676473095523
tienduy-nguyen <tienduy.nguyen.dev@gmail.com>
2024-06-02 23:32:04 +0200

    feat(tubed): create command wrapper from yt-dlp

--- projects/tubed/src/main.rs
+++ projects/tubed/src/main.rs
@@ -1,4 +1,6 @@
 use clap::Parser;
+use std::error::Error;
+use std::process::Command;
 
 #[derive(Parser)]
 #[command(
@@ -12,10 +14,16 @@ struct Cli {
     url: String,
 
     #[clap(short, long, help = "Output format: mp4 or mp3", default_value = "mp4")]
-    format: String,
+    format: Format,
 }
 
-fn main() -> Result<(), String> {
+#[derive(clap::ValueEnum, Clone, Debug)]
+enum Format {
+    Mp4,
+    Mp3,
+}
+
+fn main() -> Result<(), Box<dyn Error>> {
     let args = Cli::parse();
 
     match download_video(&args.url, &args.format) {
@@ -26,6 +34,27 @@ fn main() -> Result<(), String> {
     Ok(())
 }
 
-fn download_video(_url: &str, _format: &str) -> Result<(), String> {
-    Ok(())
+fn download_video(url: &str, format: &Format) -> Result<(), Box<dyn Error>> {
+    let mut cmd = Command::new("yt-dlp");
+    cmd.arg(url);
+
+    match format {
+        Format::Mp4 => cmd
+            .arg("--format")
+            .arg("bestvideo[ext=mp4]+bestaudio[ext=m4a]/mp4"),
+        Format::Mp3 => cmd.arg("--extract-audio").arg("--audio-format").arg("mp3"),
+    };
+
+    let output = cmd.output()?;
+
+    if output.status.success() {
+        println!("Download completed successfully.");
+        Ok(())
+    } else {
+        let error_message = String::from_utf8_lossy(&output.stderr);
+        Err(Box::new(std::io::Error::new(
+            std::io::ErrorKind::Other,
+            format!("yt-dlp failed: {}", error_message),
+        )))
+    }
 }

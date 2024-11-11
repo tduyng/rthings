e26f9d2e6345fa039d2d6fb166b0ecf312b6712a
tduyng <tduyng.dev@gmail.com>
2024-11-11 18:10:49 +0100

    feat(calc): complete simple lexer

--- projects/calc/src/lexer.rs
+++ projects/calc/src/lexer.rs
@@ -4,7 +4,7 @@ use std::{
     vec,
 };
 
-use crate::token::Token;
+use crate::token::{Token, TokenKind};
 
 #[derive(Debug)]
 pub struct Lexer {
@@ -25,18 +25,27 @@ impl Lexer {
     }
 
     pub fn lex(&mut self) -> Vec<Token> {
-        let tokens = vec![];
+        let mut tokens = vec![];
         while self.line.is_some() {
             if let Some(c) = self.char() {
                 match c {
                     ' ' | '\t' => self.advance(),
                     '#' => self.skip_comment(),
+                    '0'..='9' => tokens.push(self.lex_number()),
+                    '+' => tokens.push(self.single_char_token(TokenKind::Plus)),
+                    '-' => tokens.push(self.single_char_token(TokenKind::Minus)),
+                    '*' => tokens.push(self.single_char_token(TokenKind::Star)),
+                    '/' => tokens.push(self.single_char_token(TokenKind::Slash)),
+                    '^' => tokens.push(self.single_char_token(TokenKind::Caret)),
+                    '(' => tokens.push(self.single_char_token(TokenKind::LParen)),
+                    ')' => tokens.push(self.single_char_token(TokenKind::RParen)),
                     _ => self.advance(),
                 }
             } else {
                 self.advance_line();
             }
         }
+        dbg!(&tokens);
         tokens
     }
 
@@ -57,5 +66,48 @@ impl Lexer {
         while self.char().is_some() {
             self.advance();
         }
+    }
+
+    fn lex_number(&mut self) -> Token {
+        let start = self.pos;
+        let mut has_dot = false;
+        let mut has_exponent = false;
+
+        while let Some(c) = self.char() {
+            match c {
+                '0'..='9' | '_' => self.advance(),
+                '.' if !has_dot => {
+                    has_dot = true;
+                    self.advance();
+                }
+                'e' | 'E' if !has_exponent => {
+                    has_exponent = true;
+                    self.advance();
+                    if let Some(next_char) = self.char() {
+                        if next_char == '+' || next_char == '-' {
+                            self.advance();
+                        }
+                    }
+                }
+                _ => break,
+            }
+        }
+
+        let end = self.pos;
+        let number_str: String = self.line.as_ref().unwrap()[start..end]
+            .chars()
+            .filter(|&c| c != '_')
+            .collect();
+        let number = number_str.parse().unwrap();
+        Token {
+            pos: start,
+            kind: TokenKind::Number(number),
+        }
+    }
+
+    fn single_char_token(&mut self, kind: TokenKind) -> Token {
+        let token = Token::new(self.pos, kind);
+        self.advance();
+        token
     }
 }
--- projects/calc/src/token.rs
+++ projects/calc/src/token.rs
@@ -1,4 +1,23 @@
 #[derive(Debug)]
+pub enum TokenKind {
+    Number(f64),
+    Plus,
+    Minus,
+    Star,
+    Slash,
+    Caret,
+    LParen,
+    RParen,
+}
+
+#[derive(Debug)]
 pub struct Token {
     pub pos: usize,
+    pub kind: TokenKind,
+}
+
+impl Token {
+    pub fn new(pos: usize, kind: TokenKind) -> Self {
+        Token { pos, kind }
+    }
 }
